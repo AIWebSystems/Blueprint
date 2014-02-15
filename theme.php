59b82eda449d23824606a673adc744f07acfdbab
Ryan Thompson <service@aiwebsystems.com>
2014-02-15 03:36:17 -0600

    Initial commit

---
+++ theme.php
@@ -0,0 +1,13 @@
+<?php
+
+use Pyro\Module\Addons\AbstractTheme;
+
+class Theme_Blueprint extends AbstractTheme
+{
+    public $name = 'Blueprint';
+    public $author = 'Ryan Thompson - AI Web Systems, Inc.';
+    public $author_website = 'http://www.aiwebsystems.com/';
+    public $website = 'http://www.aiwebsystems.com/';
+    public $description = 'An HTML5 Bootstrap template using Modernizr, LESS, jQuery and more.';
+    public $version = '3.0';
+}
